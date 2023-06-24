7b617012418c6dc4737d69f9a03c81b82f050050
ankit.jain3663@gmail.com <ankit@Ankits-MBP.fios-router.home>
2023-06-24 15:24:09 -0400

    eslint rules added for best practices

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,35 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    strict: ['error', 'global'],
+    'multiline-comment-style': ['error', 'starred-block'],
+    'spaced-comment': ['error', 'always'],
+    semi: ['error', 'always'],
+    'semi-spacing': 'error',
+    'no-extra-semi': 'error',
+    'no-expected-multiline': 'error',
+    'max-len': ['error', { 'code': 80 }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'space-infix-ops': 'error',
+    'space-before-blocks': 'error',
+    'brace-style': 'error',
+    'keyword-spacing': 'error',
+    'arraow-spacing': 'error',
+    'space-before-function-paren': [
+      'error',
+      {
+        'annonymous': 'always',
+        'named': 'never',
+        'asyncArrow': 'always',
+      },
+    ],
+    'newline-per-chained-called': 'error',
+    'space-in-parens': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'no-multiply-empty-lines': ['error', { 'max' : 1, 'maxEOF' : 1 }],
   }
 }
